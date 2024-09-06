c4219c7d993d931eaa31a6c3a3bcc9db8704abf4
brandon <brandonmassie32@gmail.com>
2024-09-06 19:18:48 +0000

    test

--- src/routes/routes.analytics.js
+++ src/routes/routes.analytics.js
@@ -33,7 +33,7 @@ async function routes(fastify, options) {
 
   /**
    * @async
-   * @function getManageAnalytics
+   * @function /manage/analytics
    * @group Analytics Endpoints
    * @param {object} request - The Fastify request object.
    * @param {Object} reply - The Fastify reply object.
